5a4b241da5307e6c0bd90aebb362db3a1ba8d23b
Bret Bunny Baxter <177974481+bunny-baxter@users.noreply.github.com>
2025-01-01 17:35:09 +0100

    Make acting when the game is over a no-op instead of a crash.

--- game_model.py
+++ game_model.py
@@ -2,9 +2,6 @@ import toml
 
 config = toml.load("game_config.toml")
 
-class GameEndedException(Exception):
-    pass
-
 class InvalidActionException(Exception):
     pass
 
@@ -25,26 +22,31 @@ class CroissantGame():
 
     def execute_labor(self):
         if self.turns_left <= 0:
-            raise GameEndedException()
+            return
+
         self.money += config["labor_payoff"]
         self._end_turn()
 
     def execute_invest(self):
         if self.turns_left <= 0:
-            raise GameEndedException()
+            return
+
         if self.money < config["invest_cost"]:
             raise InvalidActionException(f'You need {config["invest_cost"]} dollars to invest but you only have {self.money} dollars.')
+
         self.money -= config["invest_cost"]
         self.investments.append(config["invest_lag_turns"])
         self._end_turn()
 
     def execute_consume(self, cost):
         if self.turns_left <= 0:
-            raise GameEndedException()
+            return
+
         if not cost in config["consume_costs"]:
             raise InvalidActionException(f'You can\'t buy {cost} croissants.')
         if self.money < cost:
             raise InvalidActionException(f'You need {cost} dollars to buy {cost} croissants, but you only have {self.money} dollars.')
+
         self.money -= cost
         self.croissants += cost
         self._end_turn()
